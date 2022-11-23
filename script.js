5cdeae0ff7d5c651f881e2ff21043d48dcd15165
anvar <anvar>
2022-11-23 17:29:59 +0500

    juft_va_toq

---
+++ script.js
@@ -0,0 +1,11 @@
+var son = +prompt("son kiriting juft yoki toqligini aniqlaymiz")
+if (son%2==0) {
+    alert("siz juft son kiritdingiz "+son) 
+    
+}
+else if (son%2==1) {
+    alert("siz toq son kiritdingiz "+son)
+}
+else{
+    alert("siz sondan boshqa narsa kiritdingiz")
+}
